60105589140e260441b5926e9f097849b18cbb24
Zdenek Styblik <stybla@turnovfree.net>
2016-06-13 14:11:42 +0200

    Allow to choose output format via CLI flags

--- cmd/facter/main.go
+++ cmd/facter/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/zstyblik/go-facter/lib/cpu"
 	"github.com/zstyblik/go-facter/lib/disk"
 	"github.com/zstyblik/go-facter/lib/facter"
+	"github.com/zstyblik/go-facter/lib/formatter"
 	"github.com/zstyblik/go-facter/lib/host"
 	"github.com/zstyblik/go-facter/lib/mem"
 	"github.com/zstyblik/go-facter/lib/net"
 )
 
 func main() {
-	facter := facter.New(nil)
+	conf := facter.FacterConfig{}
+	plainText := flag.Bool("plaintext", false,
+		"Emit facts as key => value pairs")
+	keyValue := flag.Bool("keyvalue", false,
+		"Emit facts as key:value pairs")
+	flag.Parse()
+
+	if *plainText == true {
+		conf.Formatter = formatter.NewFormatter()
+	} else if *keyValue == true {
+		conf.Formatter = formatter.NewKeyValueFormatter()
+	} else {
+		conf.Formatter = formatter.NewFormatter()
+	}
+
+	facter := facter.New(&conf)
 	_ = cpu.GetCPUFacts(facter)
 	_ = disk.GetDiskFacts(facter)
 	_ = host.GetHostFacts(facter)

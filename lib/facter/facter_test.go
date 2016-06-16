64bf7928dd4d41c6ad9e82abba0e9a3e2fdeb2c0
Zdenek Styblik <stybla@turnovfree.net>
2016-06-16 16:24:22 +0200

    More tests for lib/facter/facter.go

--- lib/facter/facter_test.go
+++ lib/facter/facter_test.go
@@ -1,14 +1,16 @@
 package facter
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/zstyblik/go-facter/lib/formatter"
 )
 
 func TestNewNilConf(t *testing.T) {
-	facter := New(nil)
-	if facter == nil {
+	f := New(nil)
+	if f == nil {
 		t.Fail()
 	}
 }
@@ -17,8 +19,27 @@ func TestNewConf(t *testing.T) {
 	conf := Config{
 		Formatter: formatter.NewFormatter(),
 	}
-	facter := New(&conf)
-	if facter == nil {
+	f := New(&conf)
+	if f == nil {
 		t.Fail()
+	}
+}
+
+func TestFacter(t *testing.T) {
+	testKey := "test"
+	testValue := "test value"
+	f := New(nil)
+	if f == nil {
+		t.Fail()
+	}
+	f.Add(testKey, testValue)
+	value, ok := f.Get(testKey)
+	if ok == false || strings.Compare(fmt.Sprintf("%v", value), testValue) != 0 {
+		t.Fatalf("Failed to get K/V: %v:%v:%v", testKey, value, ok)
+	}
+	f.Delete(testKey)
+	value, ok = f.Get(testKey)
+	if ok != false {
+		t.Fatalf("Got %v, value %v", ok, value)
 	}
 }

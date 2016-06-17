c57d664cf45b931a44992a83ff56b9de965ab375
Zdenek Styblik <stybla@turnovfree.net>
2016-06-17 11:59:03 +0200

    Add unit test targeted at Formatter call, resp. Facter.Print()

--- lib/facter/facter_test.go
+++ lib/facter/facter_test.go
@@ -8,23 +8,6 @@ import (
 	"github.com/zstyblik/go-facter/lib/formatter"
 )
 
-func TestNewNilConf(t *testing.T) {
-	f := New(nil)
-	if f == nil {
-		t.Fail()
-	}
-}
-
-func TestNewConf(t *testing.T) {
-	conf := Config{
-		Formatter: formatter.NewFormatter(),
-	}
-	f := New(&conf)
-	if f == nil {
-		t.Fail()
-	}
-}
-
 func TestFacter(t *testing.T) {
 	testKey := "test"
 	testValue := "test value"
@@ -41,5 +24,67 @@ func TestFacter(t *testing.T) {
 	value, ok = f.Get(testKey)
 	if ok != false {
 		t.Fatalf("Got %v, value %v", ok, value)
+	}
+}
+
+type FakeFormatter struct {
+	facts map[string]interface{}
+}
+
+func (ff *FakeFormatter) Get(k string) (interface{}, bool) {
+	val, ok := ff.facts[k]
+	return val, ok
+}
+
+func NewFakeFormatter() *FakeFormatter {
+	f := FakeFormatter{}
+	f.facts = make(map[string]interface{})
+	return &f
+}
+
+func (ff *FakeFormatter) Print(facts map[string]interface{}) error {
+	for k, v := range facts {
+		ff.facts[k] = v
+	}
+
+	return nil
+}
+
+func TestFacterFormatter(t *testing.T) {
+	testKey := "test"
+	testValue := "test_value"
+	ff := NewFakeFormatter()
+	conf := &Config{
+		Formatter: ff,
+	}
+	f := New(conf)
+	if f == nil {
+		t.Fatal()
+	}
+	f.Add(testKey, testValue)
+	f.Print()
+	val, ok := ff.Get(testKey)
+	if ok != true {
+		t.Fatal()
+	}
+	if strings.Compare(fmt.Sprintf("%v", val), testValue) != 0 {
+		t.Fatal()
+	}
+}
+
+func TestNewNilConf(t *testing.T) {
+	f := New(nil)
+	if f == nil {
+		t.Fail()
+	}
+}
+
+func TestNewConf(t *testing.T) {
+	conf := Config{
+		Formatter: formatter.NewFormatter(),
+	}
+	f := New(&conf)
+	if f == nil {
+		t.Fail()
 	}
 }

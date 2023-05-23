cfbfdc47d8fc53aac08873c24be0cdd87c500d53
itzwam <26522723+itzwam@users.noreply.github.com>
2023-05-23 18:42:00 +0200

    Bird

--- lib/facters/routehOS/bird.go
+++ lib/facters/routehOS/bird.go
@@ -14,17 +14,18 @@ func GetBirdFacts(f facter.IFacter) error {
 	cmd := exec.Command("bird", "--version")
 
 	// Run the command and retrieve the output
-	output, err := cmd.Output()
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
-	f.Add("bird_version_stdout", string(output))
 
 	result := reBirdVersion.FindStringSubmatch(string(output))
 
 	debug("%v", result)
 
-	f.Add("bird_version", strings.Join(result, " "))
+	if len(result) > 0 {
+		f.Add("bird_version", strings.Join(result, " "))
+	}
 
 	return nil
 }
